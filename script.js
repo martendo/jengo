25e3e366bb2692accc556e7641c15689b4dc78d2
martendo <51766598+martendo@users.noreply.github.com>
2022-04-08 20:15:10 -0400

    Create basic layout

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-ctx.fillStyle = "#ffe0b0";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+function redrawCanvas() {
+	ctx.strokeStyle = "#000000";
+	ctx.lineWidth = 1;
+	ctx.strokeRect(canvas.width / 2 - 50, canvas.height / 2 - 50, 100, 100);
+}
+
+function resizeCanvas() {
+	canvas.width = canvas.clientWidth;
+	canvas.height = canvas.clientHeight;
+	redrawCanvas();
+}
+
+window.addEventListener("resize", resizeCanvas);
+
+resizeCanvas();
