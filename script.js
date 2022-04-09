47c1b7e9b1ce93b58308fab4c1aea00e7719285c
martendo <51766598+martendo@users.noreply.github.com>
2022-04-08 21:08:51 -0400

    Add game code and help buttons

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+let gameCode = null;
+
+function copyText(text) {
+	navigator.clipboard.writeText(text).catch(() => {
+		console.log("navigator.clipboard.writeText failed");
+		const textarea = document.createElement("textarea");
+		textarea.style.position = "fixed";
+		textarea.style.top = "-1000px";
+		textarea.value = text;
+		document.body.appendChild(textarea);
+		textarea.select();
+		document.execCommand("copy");
+		document.body.removeChild(textarea);
+	});
+}
+
 function redrawCanvas() {
 	ctx.strokeStyle = "#000000";
 	ctx.lineWidth = 1;
@@ -16,3 +32,13 @@ function resizeCanvas() {
 window.addEventListener("resize", resizeCanvas);
 
 resizeCanvas();
+
+document.getElementById("game-code").addEventListener("click", () => copyText(`${window.location.origin}${window.location.pathname}#${encodeURIComponent(gameCode)}`));
+
+const help = document.getElementById("help");
+document.getElementById("help-btn").addEventListener("click", () => {
+	if (help.style.display === "")
+		help.style.display = "unset";
+	else
+		help.style.display = "";
+});
